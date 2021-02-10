3e3b875da662bdcfee768c3ebb0f93e361629279
Eric Leung <eric@erictleung.com>
2021-02-10 13:36:34 -0800

    Start data documentation

---
+++ R/data.R
@@ -0,0 +1,27 @@
+#' Pixar films
+#'
+#' A dataset containing Pixar films, their release order, and release date.
+#'
+#' @format A data frame with 27 rows and 3 variables:
+#' \describe{
+#'   \item{number}{order of release}
+#'   \item{film}{name of film}
+#'   \item{release_date}{date film premiered}
+#' }
+#' @source \url{https://en.wikipedia.org/wiki/List_of_Pixar_films}
+"pixar_films"
+
+
+#' People behind Pixar
+#'
+#' A dataset containing the main people involved in each Pixar film.
+#'
+#' @format A data frame with 228 rows and 3 variables:
+#' \describe{
+#'   \item{film}{name of film}
+#'   \item{role_type}{one of five roles: Director, Musician, Producer,
+#'     Screenwriter, Storywriter}
+#'   \item{name}{individual's name}
+#' }
+#' @source \url{https://en.wikipedia.org/wiki/List_of_Pixar_films}
+"pixar_people"
