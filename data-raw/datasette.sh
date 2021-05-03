7c4774ac2ad84899582105aa1c76ee66dd5f597d
Eric Leung <eric@erictleung.com>
2021-05-02 18:28:19 -0700

    Make datasette script more robust to updating

--- data-raw/datasette.sh
+++ data-raw/datasette.sh
@@ -2,13 +2,18 @@
 
 # Setup datasette deployment for pixarfilms to Heroku
 # Author: Eric T Leung
-# Last ran: 2021-04-13
+# Created: 2021-04-13
+# Last ran: 2021-05-02
 # Run: bash datasette.sh
 
 # Installation
 # pip3 install datasette csvs-to-sqlite
 
 # Convert CSV files to SQLite3 database file
+FILE=pixarfilms.db
+if [ -f "$FILE" ]; then
+    rm $FILE
+fi
 csvs-to-sqlite *.csv pixarfilms.db
 
 # Publish to Heroku
