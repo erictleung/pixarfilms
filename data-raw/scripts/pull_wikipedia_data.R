5b8a9225af36dc9fecadb6265555d8ed485c7f9c
Eric Leung <2754821+erictleung@users.noreply.github.com>
2026-04-02 17:56:44 -0400

    feat: add wikipedia pull script and staging DB

---
+++ data-raw/scripts/pull_wikipedia_data.R
@@ -0,0 +1,27 @@
+library(rvest)
+library(dplyr)
+library(DBI)
+library(RSQLite)
+library(janitor)
+library(here)
+
+# Connect to staging database
+message("Connecting to staging database...")
+con <- dbConnect(SQLite(), here("data-raw", "pixar_staging.db"))
+
+# Scrape Wikipedia page
+message("Scraping Wikipedia page...")
+page <- read_html("https://en.wikipedia.org/wiki/List_of_Pixar_films")
+tbls <- html_table(page, fill = TRUE)
+
+# Extract and save tables
+message("Saving raw tables to staging database...")
+banner_offset <- 0 # Adjust if Wikipedia adds banners
+dbWriteTable(con, "raw_wiki_films", tbls[[1 + banner_offset]] |> clean_names(), overwrite = TRUE)
+dbWriteTable(con, "raw_wiki_box_office", tbls[[3 + banner_offset]] |> clean_names(), overwrite = TRUE)
+dbWriteTable(con, "raw_wiki_public_response", tbls[[4 + banner_offset]] |> clean_names(), overwrite = TRUE)
+dbWriteTable(con, "raw_wiki_academy", tbls[[5 + banner_offset]] |> clean_names(), overwrite = TRUE)
+
+message("Success! Tables written to staging database.")
+# Clean up
+dbDisconnect(con)
