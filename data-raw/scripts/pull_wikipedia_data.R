d9f0c33274695d92f492f5b162c22bf4032632ec
Eric Leung <2754821+erictleung@users.noreply.github.com>
2026-04-15 23:05:42 -0400

    chore: add comments to initial pull of Wikipedia

--- data-raw/scripts/pull_wikipedia_data.R
+++ data-raw/scripts/pull_wikipedia_data.R
@@ -1,3 +1,12 @@
+# Pull tables with data from Wikipedia
+# Tables include:
+# - Pixar films
+# - Box office
+# - Public response
+# - Academy awards
+
+
+# Load libraries
 library(rvest)
 library(dplyr)
 library(DBI)
@@ -5,14 +14,17 @@ library(RSQLite)
 library(janitor)
 library(here)
 
+
 # Connect to staging database
 message("Connecting to staging database...")
 con <- dbConnect(SQLite(), here("data-raw", "pixar_staging.db"))
+
 
 # Scrape Wikipedia page
 message("Scraping Wikipedia page...")
 page <- read_html("https://en.wikipedia.org/wiki/List_of_Pixar_films")
 tbls <- html_table(page, fill = TRUE)
+
 
 # Extract and save tables
 message("Saving raw tables to staging database...")
@@ -21,7 +33,8 @@ dbWriteTable(con, "raw_wiki_films", tbls[[1 + banner_offset]] |> clean_names(), 
 dbWriteTable(con, "raw_wiki_box_office", tbls[[3 + banner_offset]] |> clean_names(), overwrite = TRUE)
 dbWriteTable(con, "raw_wiki_public_response", tbls[[4 + banner_offset]] |> clean_names(), overwrite = TRUE)
 dbWriteTable(con, "raw_wiki_academy", tbls[[5 + banner_offset]] |> clean_names(), overwrite = TRUE)
-
 message("Success! Tables written to staging database.")
+
+
 # Clean up
 dbDisconnect(con)
