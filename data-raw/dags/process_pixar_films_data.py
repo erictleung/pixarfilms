d9118934a6468f05943e4510799db999ff516f06
Eric Leung <2754821+erictleung@users.noreply.github.com>
2026-04-17 02:02:58 -0400

    fix: Airflow DAG to work with basic syntax checks

--- data-raw/dags/process_pixar_films_data.py
+++ data-raw/dags/process_pixar_films_data.py
@@ -1,7 +1,6 @@
 from datetime import datetime
-from airflow import DAG
 from airflow.providers.standard.operators.bash import BashOperator
-from airflow.utils.task_group import TaskGroup
+from airflow.sdk import DAG, task, task_group
 
 default_args = {
     "owner": "admin",
@@ -15,42 +14,46 @@ with DAG(
     default_args=default_args,
     tags=['pixar', 'rstats', 'webscraping']
 ) as dag:
-    with TaskGroup("pull_tasks") as pull_tasks:
-        p_wiki = BashOperator(
-            task_id="pull_wikipedia", 
+    @task_group()
+    def pull_tasks():
+        p_films = BashOperator(
+            task_id="pull_wikipedia",
             bash_command="Rscript data-raw/scripts/pull_wikipedia_data.R"
         )
         p_omdb = BashOperator(
-            task_id="pull_omdb", 
+            task_id="pull_omdb",
             bash_command="Rscript data-raw/scripts/pull_omdb_data.R"
         )
         p_rt = BashOperator(
-            task_id="pull_rt", 
+            task_id="pull_rt",
             bash_command="Rscript data-raw/scripts/pull_rt_ratings.R"
         )
-
-    with TaskGroup("clean_tasks") as clean_tasks:
+        
+        p_films >> p_omdb >> p_rt
+    
+    @task_group
+    def clean_tasks():
         c_films = BashOperator(
-            task_id="clean_films", 
+            task_id="clean_films",
             bash_command="Rscript data-raw/scripts/clean_pixar_films.R"
         )
         c_people = BashOperator(
-            task_id="clean_people", 
+            task_id="clean_people",
             bash_command="Rscript data-raw/scripts/clean_pixar_people.R"
         )
         c_box = BashOperator(
-            task_id="clean_box_office", 
+            task_id="clean_box_office",
             bash_command="Rscript data-raw/scripts/clean_box_office.R"
         )
         c_pub = BashOperator(
-            task_id="clean_public_response", 
+            task_id="clean_public_response",
             bash_command="Rscript data-raw/scripts/clean_public_response.R"
         )
         c_acad = BashOperator(
-            task_id="clean_academy", 
+            task_id="clean_academy",
             bash_command="Rscript data-raw/scripts/clean_academy.R"
         )
         
         c_films >> c_people >> c_box >> c_pub >> c_acad
-
-    pull_tasks >> clean_tasks
+    
+    pull_tasks() >> clean_tasks()
