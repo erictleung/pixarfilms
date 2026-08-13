08dfc6a6f3825b3ab734e51d65e04bd8acbac2c7
Eric Leung <2754821+erictleung@users.noreply.github.com>
2026-08-13 00:51:53 -0400

    refactor: updated generation script

--- data-raw/datasette.sh
+++ data-raw/datasette.sh
@@ -3,18 +3,27 @@
 # Setup datasette deployment for pixarfilms to Heroku
 # Author: Eric T Leung
 # Created: 2021-04-13
-# Last ran: 2021-05-02
+# Last ran: 2026-08-12
 # Run: bash datasette.sh
 
 # Installation
 # pip3 install datasette csvs-to-sqlite
 
 # Convert CSV files to SQLite3 database file
-FILE=pixarfilms.db
+echo "Converting CSV files into a SQLite3 database file..."
+FILE=pixarfilms-datasette/pixarfilms.db
 if [ -f "$FILE" ]; then
     rm $FILE
 fi
-csvs-to-sqlite *.csv pixarfilms.db
+csvs-to-sqlite *.csv pixarfilms-datasette/pixarfilms.db
+echo "Done!"
 
 # To serve locally
+# cd pixarfilms-datasette
 # datasette serve pixarfilms.db
+
+# To serve on Vercel preview
+# vercel
+
+# To serve on Vercel production
+# vercel --prod
